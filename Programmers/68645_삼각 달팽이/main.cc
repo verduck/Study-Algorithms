b20a15cae33b00a4aad3ce6f805c9d1799190624
jeyog <choijy8816@gmail.com>
2022-02-20 14:45:30 +0900

    프로그래머스 68645번: 삼각 달팽이 풀이 완료

---
+++ Programmers/68645_삼각 달팽이/main.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void TriangleSnail(vector<vector<int>>& triangle, int sx, int sy, int num, int size) {
+    if (size <= 0) {
+        return;    
+    }
+
+    int x = sx;
+    int y = sy;
+    for (int i = y; i < size + sy; i++) {
+        triangle[i][x] = num++;
+    }
+    y = size + sy - 1;
+    x++;
+    for (int i = x; i < size + sx; i++) {
+        triangle[y][i] = num++;
+    }
+    x = size + sx - 1;
+    y--;
+    x--;
+    for (int i = y, j = x; i > sy && j > sx; i--, j--) {
+        triangle[i][j] = num++;
+        x = j;
+        y = i;
+    }
+
+    TriangleSnail(triangle, x, y + 1, num, size - 3);
+}
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    vector<vector<int>> triangle(n);
+
+    for (int i = 0; i < n; i++) {
+        triangle[i].assign(i + 1, 0);
+    }
+
+    TriangleSnail(triangle, 0, 0, 1, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j<= i; j++) {
+            answer.push_back(triangle[i][j]);
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    int n = 6;
+    vector<int> result = solution(n);
+    for (int i : result) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
