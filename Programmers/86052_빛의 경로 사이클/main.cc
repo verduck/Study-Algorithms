0c218afdc925425488c752615fc9b36149773c02
cwooder <choijy8816@gmail.com>
2022-02-27 23:19:24 +0900

    프로그래머스 86052번: 빛의 경로 사이클 풀이 완료

--- Programmers/86052_빛의 경로 사이클/main.cc
+++ Programmers/86052_빛의 경로 사이클/main.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <tuple>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,37 +14,61 @@ const vector<pair<int, int>> direction = {
     { -1, 0 }
 };
 
-int cycle(const vector<string>& grid, int sx, int sy, int d) {
+int cycle(const vector<string>& grid, vector<vector<vector<bool>>>& visited, int x, int y, int d) {
     int len = 0;
     queue<tuple<int, int, int>> q;
-    vector<vector<vector<bool>>> visited(grid.size(), vector<vector<bool>>(grid[0].size(), vector<bool>(4, false)));
-    
+
+    q.push({ x, y, d });
+
     while (!q.empty()) {
         tuple<int, int, int> t = q.front();
         q.pop();
         int cx = get<0>(t);
         int cy = get<1>(t);
         int cd = get<2>(t);
+        visited[cy][cx][cd] = true;
+        len++;
+
+        int nd;
+        if (grid[cy][cx] == 'S') {
+            nd = cd;
+        } else if (grid[cy][cx] == 'L') {
+            nd = (cd + 1) % direction.size();
+        } else if (grid[cy][cx] == 'R') {
+            nd = (cd + 3) % direction.size();
+        }
+        int ny = (cy + direction[nd].second + grid.size()) % grid.size();
+        int nx = (cx + direction[nd].first + grid[ny].size()) % grid[ny].size();
+
+        if (!visited[ny][nx][nd]) {
+            q.push({ nx, ny, nd });
+        }
     }
-    
-    return -1;
+
+    return len;
 }
 
 vector<int> solution(vector<string> grid) {
     vector<int> answer;
+    vector<vector<vector<bool>>> visited(grid.size(), vector<vector<bool>>(grid[0].size(), vector<bool>(4, false)));
 
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
             for (int k = 0; k < direction.size(); k++) {
-                cycle(grid, j, i, k);
+                if (!visited[i][j][k]) {
+                    int c = cycle(grid, visited, j, i, k);
+                    answer.push_back(c);
+                }
             }
         }
     }
+
+    sort(answer.begin(), answer.end());
     return answer;
 }
 
 int main() {
-    vector<string> grid = { "SL", "LR" };
+    vector<string> grid = { "SL","LR" };
     vector<int> result = solution(grid);
     for (int i : result) {
         cout << i << ' ';
