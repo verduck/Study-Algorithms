8b5e81443dfbc21dcc29b8c25d3d64cc30269dd0
cwooder <choijy8816@gmail.com>
2022-03-03 22:09:53 +0900

    프로그래머스 12982번: 예산 풀이 완료

---
+++ Programmers/12982_예산/main.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for (int i = 0; i < d.size(); i++) {
+        answer += d[i];
+        if (answer > budget) {
+            return i;
+        }
+    }
+    return d.size();
+}
+
+int main(void) {
+    vector<int> d = { 2, 2, 3, 3 };
+    int budget = 10;
+    int answer = solution(d, budget);
+    cout << answer << '\n';
+    return 0;
+}
