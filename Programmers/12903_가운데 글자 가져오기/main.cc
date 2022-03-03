4d0638a5d2c2f739b1ff46fb4fe20dc6e9d8e187
cwooder <choijy8816@gmail.com>
2022-03-03 15:22:40 +0900

    프로그래머스 12903번: 가운데 글자 가져오기 풀이 완료

---
+++ Programmers/12903_가운데 글자 가져오기/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string solution(string s) {
+    string answer;
+    if (s.size() % 2) {
+        answer = s.substr(s.size() / 2, 1);
+    } else {
+        answer = s.substr(s.size() / 2 - 1, 2);
+    }
+    return answer;
+}
+
+int main() {
+    string s = "qwer";
+    string answer = solution(s);
+    cout << answer << '\n';
+    return 0;
+}
