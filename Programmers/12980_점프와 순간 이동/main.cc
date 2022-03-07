e8930c4ff63363487e3411b3a09c6693b9d10d90
cwooder <choijy8816@gmail.com>
2022-03-07 15:17:42 +0900

    프로그래머스 12980번: 점프와 순간 이동 풀이 완료

--- Programmers/12980_점프와 순간 이동/main.cc
+++ Programmers/12980_점프와 순간 이동/main.cc
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
 int solution(int n) {
     int ans = 0;
-    
-    while (n) {
-        if (n % 2) {
-            ans++;
-        }
-        n /= 2;
-    }
+    bitset<32> b(n);
+
+    ans = b.count();
 
     return ans;
 }
