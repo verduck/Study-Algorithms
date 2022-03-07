cd11b0985e5b37f7b35cfaca5504bdca415d1e7a
cwooder <choijy8816@gmail.com>
2022-03-07 22:26:20 +0900

    프로그래머스 87377번: 교점에 별 만들기 풀이 완료

--- Programmers/87377_교점에 별 만들기/main.cc
+++ Programmers/87377_교점에 별 만들기/main.cc
@@ -6,62 +6,60 @@
 
 using namespace std;
 
-vector<long> meet(const vector<int>& f1, const vector<int>& f2) {
-    long x, y;
+vector<int> meet(const vector<int>& f1, const vector<int>& f2) {
+    long x_numerator = static_cast<long>(f1[1]) * f2[2] - static_cast<long>(f1[2]) * f2[1];
+    long x_denominator = static_cast<long>(f1[0]) * f2[1] - static_cast<long>(f1[1]) * f2[0];
+    long y_numerator = static_cast<long>(f1[2]) * f2[0] - static_cast<long>(f1[0]) * f2[2];
+    long y_denominator = static_cast<long>(f1[0]) * f2[1] - static_cast<long>(f1[1]) * f2[0];
 
-    long denominator = static_cast<long>(f1[0]) * f2[1] - static_cast<long>(f1[1]) * f2[0];
-    long x_molecule = static_cast<long>(f1[1]) * f2[2] - static_cast<long>(f1[2]) * f2[1];
-    long y_molecule = static_cast<long>(f1[2]) * f2[0] - static_cast<long>(f1[0]) * f2[2];
-
-    if (denominator == 0) {
-        return vector<long>();
+    if (x_denominator == 0 || y_denominator == 0) {
+        return vector<int>();
     }
 
-    if ((x_molecule % denominator) || (y_molecule % denominator)) {
-        return vector<long>();
+    double x = x_numerator / x_denominator;
+    double y = y_numerator / y_denominator;
+
+    if (!(x_numerator % x_denominator) && !(y_numerator % y_denominator)) {
+        return { static_cast<int>(x), static_cast<int>(y) };
     }
 
-    x = x_molecule / denominator;
-    y = y_molecule / denominator;
-
-    return { x, y };
+    return vector<int>();
 }
 
 vector<string> solution(vector<vector<int>> line) {
     vector<string> answer;
-    vector<vector<long>> meets;
-
-    long max_y = LONG_MIN;
-    long min_y = LONG_MAX;
-    long max_x = LONG_MIN;
-    long min_x = LONG_MAX;
+    vector<vector<int>> meets;
+    int max_x = INT_MIN;
+    int min_x = INT_MAX;
+    int max_y = INT_MIN;
+    int min_y = INT_MAX;
 
     for (int i = 0; i < line.size() - 1; i++) {
         for (int j = i + 1; j < line.size(); j++) {
-            vector<long> v = meet(line[i], line[j]);
-            if (!v.empty()) {
-                max_y = max(max_y, v[1]);
-                min_y = min(min_y, v[1]);
-                max_x = max(max_x, v[0]);
-                min_x = min(min_x, v[0]);
-                meets.push_back(v);
+            vector<int> m = meet(line[i], line[j]);
+            if (!m.empty()) {
+                meets.push_back(m);
+                max_x = max(max_x, m[0]);
+                min_x = min(min_x, m[0]);
+                max_y = max(max_y, m[1]);
+                min_y = min(min_y, m[1]);
             }
         }
     }
 
     answer.assign(max_y - min_y + 1, string(max_x - min_x + 1, '.'));
 
-    for (const vector<long>& v : meets) {
+    for (const vector<int>& v : meets) {
         int x = abs(min_x - v[0]);
         int y = abs(max_y - v[1]);
         answer[y][x] = '*';
     }
-    
+
     return answer;
 }
 
 int main() {
-    vector<vector<int>> line = { { 2, -1, 4 }, { -2, -1, 4 }, { 0, -1, 1 }, { 5, -8, -12 }, { 5, 8, 12 } };
+    vector<vector<int>> line = { { 0, 1, -1 }, { 1, 0, -1 }, { 1, 0, 1 } };
     vector<string> result = solution(line);
     for (const string& s : result) {
         cout << s << '\n';
