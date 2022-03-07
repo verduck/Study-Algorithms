636ac663247b038ba79bcad6bbef01e8e367c0a8
cwooder <choijy8816@gmail.com>
2022-03-07 15:14:44 +0900

    프로그래머스 87390번: n^2 배열 자르기 풀이 완료

--- Programmers/87390_n^2 배열 자르기/main.cc
+++ Programmers/87390_n^2 배열 자르기/main.cc
@@ -6,24 +6,23 @@ using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-    int start_row = left / n;
-    int start_col = left % n;
+    int sr = left / n;
+    int er = right / n;
+    int sc = left % n;
+    int ec = right % n;
 
-    int end_row = right / n;
-    int end_col = right % n;
-
-    for (int i = start_row; i <= end_row; i++) {
+    for (int i = sr; i <= er; i++) {
         int m = i + 1;
         for (int j = 0; j < m; j++) {
             answer.push_back(m);
         }
+
         for (int j = m + 1; j <= n; j++) {
             answer.push_back(j);
         }
     }
 
-    answer.assign(answer.begin() + start_col, answer.end() - (n - end_col) + 1);
-
+    answer.assign(answer.begin() + sc, answer.end() - (n - ec) + 1);
     return answer;
 }
 
