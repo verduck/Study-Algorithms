a8c518483a69411e320043377f89bbfa7b570651
cwooder <choijy8816@gmail.com>
2022-03-07 16:57:40 +0900

    프로그래머스 25140번: 모음사전 풀이 완료

--- Programmers/25140_모음사전/main.cc
+++ Programmers/25140_모음사전/main.cc
@@ -6,7 +6,38 @@
 
 using namespace std;
 
+const vector<char> v = { 'A', 'E', 'I', 'O', 'U' };
+
+bool dfs(const string& word, string& s, int index, int *answer) {
+    if (index > v.size()) {
+        (*answer)--;
+        return false;
+    }
+
+    if (s == word) {
+        return true;
+    }
+    
+    s += 'A';
+    for (int i = 0; i < v.size(); i++) {
+        s[index] = v[i];
+        (*answer)++;
+        if (dfs(word, s, index + 1, answer)) {
+            return true;
+        }
+    }
+    s.pop_back();
+    return false;
+}
+
 int solution(string word) {
+    int answer = 0;
+    string s;
+    dfs(word, s, 0, &answer);
+    return answer;
+}
+
+/* int solution(string word) {
     int answer = 0;
     const unordered_map<char, int> alphabets = {
         { 'A', 0 },
@@ -15,23 +46,23 @@ int solution(string word) {
         { 'O', 3 },
         { 'U', 4 },
     };
-    const int NUMBER_OF_ALPHABETS = alphabets.size();
+    const int kNumberofAlphabets = alphabets.size();
     int end_of_words = 0;
 
-    for (int i = 1; i <= NUMBER_OF_ALPHABETS; i++) {
-        end_of_words += pow(NUMBER_OF_ALPHABETS, i);
+    for (int i = 1; i <= kNumberofAlphabets; i++) {
+        end_of_words += pow(kNumberofAlphabets, i);
     }
 
     for (int i = 0; i < word.size(); i++) {
-        int n = end_of_words / pow(NUMBER_OF_ALPHABETS, i + 1);
+        int n = end_of_words / pow(kNumberofAlphabets, i + 1);
         n *= alphabets.at(word[i]);
         answer += ++n;
     }
     return answer;
-}
+} */
 
 int main() {
-    string word = "AAAAE";
+    string word = "EIO";
     int result = solution(word);
     cout << result << '\n';
     return 0;
