12acb73ebdd1557ff3cfa9c58cd65a5894baf4da
cwooder <choijy8816@gmail.com>
2022-03-08 14:30:01 +0900

    프로그래머스 12981번: 영어 끝말잇기 풀이 완료

---
+++ Programmers/12981_영어 끝말잇기/main.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer(2, 0);
+    set<string> history;
+    char prev = words[0][0];
+    int turn = 0;
+
+    for (const string& s : words) {
+        if (prev == s[0] && history.find(s) == history.end()) {
+            history.insert(s);
+            prev = s[s.size() - 1];
+            turn++;
+        } else {
+            cout << turn << '\n';
+            answer[0] = (turn % n) + 1;
+            answer[1] = (turn / n) + 1;
+            break;
+        }
+    }
+
+    return answer;
+}
+
+int main(void) {
+    int n = 2;
+    vector<string> words = { "hello", "one", "even", "never", "now", "world", "draw" };
+    vector<int> answer = solution(n, words);
+    for (int i : answer) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
