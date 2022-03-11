14e75d9fd35da18e1e8588c0248534940e0adf5d
cwooder <choijy8816@gmail.com>
2022-03-11 17:54:07 +0900

    프로그래머스 86052번: 빛의 경로 사이클 풀이 완료

--- Programmers/86052_빛의 경로 사이클/main.cc
+++ Programmers/86052_빛의 경로 사이클/main.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <tuple>
 #include <queue>
+#include <tuple>
 #include <algorithm>
 
 using namespace std;
@@ -14,56 +14,58 @@ const vector<pair<int, int>> direction = {
     { -1, 0 }
 };
 
-int cycle(const vector<string>& grid, vector<vector<vector<bool>>>& visited, int x, int y, int d) {
+int bfs(const vector<string>& grid, vector<vector<vector<bool>>>& visited, int x, int y, int d) {
     int len = 0;
     queue<tuple<int, int, int>> q;
-
     q.push({ x, y, d });
 
     while (!q.empty()) {
         tuple<int, int, int> t = q.front();
         q.pop();
-        int cx = get<0>(t);
-        int cy = get<1>(t);
-        int cd = get<2>(t);
-        visited[cy][cx][cd] = true;
+        x = get<0>(t);
+        y = get<1>(t);
+        d = get<2>(t);
+        visited[y][x][d] = true;
         len++;
 
         int nd;
-        if (grid[cy][cx] == 'S') {
-            nd = cd;
-        } else if (grid[cy][cx] == 'L') {
-            nd = (cd + 1) % direction.size();
-        } else if (grid[cy][cx] == 'R') {
-            nd = (cd + 3) % direction.size();
+        if (grid[y][x] == 'S') {
+            nd = d;
+        } else if (grid[y][x] == 'R') {
+            nd = (d + 3) % direction.size();
+        } else if (grid[y][x] == 'L') {
+            nd = (d + 1) % direction.size();
         }
-        int ny = (cy + direction[nd].second + grid.size()) % grid.size();
-        int nx = (cx + direction[nd].first + grid[ny].size()) % grid[ny].size();
+
+        int ny = (y + direction[nd].second + grid.size()) % grid.size();
+        int nx = (x + direction[nd].first + grid[ny].size()) % grid[ny].size();
 
         if (!visited[ny][nx][nd]) {
             q.push({ nx, ny, nd });
         }
     }
 
+
     return len;
 }
 
 vector<int> solution(vector<string> grid) {
     vector<int> answer;
-    vector<vector<vector<bool>>> visited(grid.size(), vector<vector<bool>>(grid[0].size(), vector<bool>(4, false)));
-
+    vector<vector<vector<bool>>> visited(grid.size(), vector<vector<bool>>(grid[0].size(), vector<bool>(direction.size(), false)));
+    
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
             for (int k = 0; k < direction.size(); k++) {
                 if (!visited[i][j][k]) {
-                    int c = cycle(grid, visited, j, i, k);
-                    answer.push_back(c);
+                    int l = bfs(grid, visited, j, i, k);
+                    answer.push_back(l);
                 }
             }
         }
     }
 
     sort(answer.begin(), answer.end());
+
     return answer;
 }
 
