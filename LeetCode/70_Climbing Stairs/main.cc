b5fda76b68d7370f6412196a3823c6f599f07c2a
jeyog <choijy8816@gmail.com>
2022-09-09 20:56:45 +0900

    LeetCode 70번: Climbing Stairs 풀이 완료

---
+++ LeetCode/70_Climbing Stairs/main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+
+void init();
+
+int main() {
+    int n = 3;
+
+    init();
+
+    Solution solution;
+    int result = solution.climbStairs(n);
+
+    cout << result << '\n';
+}
+
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
