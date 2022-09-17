48c79c42726889ceaf8013cbe2bf1367a7919644
jeyog <choijy8816@gmail.com>
2022-09-17 15:29:21 +0900

    LeetCode 141번: Linked List Cycle 풀이 완료

---
+++ LeetCode/141_Linked List Cycle/main.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) {
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (slow != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+void init();
+
+int main() {
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
+
+    init();
+
+    Solution solution;
+    bool result = solution.hasCycle(head);
+
+    cout << boolalpha << result << '\n';
+}
+
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
