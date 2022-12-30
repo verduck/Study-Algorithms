621fc68316f1b7ed2d5ca187afd6c8006e6b8bd0
jeyog <choijy8816@gmail.com>
2022-12-30 21:32:46 +0900

    프로그래머스 133502번: 햄버거 만들기 풀이 완료

---
+++ Programmers/133502_햄버거 만들기/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    const vector<int> hamburger = { 1, 2, 3, 1 };
+    vector<int> v;
+    
+    for (int i : ingredient) {
+        v.push_back(i);
+        if (v.size() >= 4) {
+            vector<int> temp(v.end() - 4, v.end());
+            if (temp == hamburger) {
+                answer++;
+                v.assign(v.begin(), v.end() - 4);
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> ingredient = { 2, 1, 1, 2, 3, 1, 2, 3, 1 };
+    int result = solution(ingredient);
+    cout << result << '\n';
+    return 0;
+}
